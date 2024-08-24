2a0fdf2fb65c26364ad006ec074e473ff030f702
ScottTylerTech <scott.shirley@tylertech.com>
2024-08-24 14:21:24 -0600

    chore: Add @tylertech/tyler-icons dependency and update user-list component

--- src/app/components/room/user-list/user-list.component.ts
+++ src/app/components/room/user-list/user-list.component.ts
@@ -1,17 +1,42 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
-import { ForgeButtonModule, ForgeCardModule } from '@tylertech/forge-angular';
-
-
-const forgeModules = [ ForgeButtonModule, ForgeCardModule];
+import { ForgeButtonModule, ForgeCardModule, ForgeIconModule } from '@tylertech/forge-angular';
+import { IconRegistry } from '@tylertech/forge';
+import { tylIconCheckCircle, tylIconRadioButtonUnchecked } from '@tylertech/tyler-icons/standard';
+const forgeModules = [ ForgeButtonModule, ForgeCardModule, ForgeIconModule];
 
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [...forgeModules],
+  imports: [...forgeModules, NgStyle],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent {
+  users: IUser[] = [
+    { name: 'John Doe', uid: '1', avatar: 'JD', hasVoted: true },
+    { name: 'Janasdasdasdasde Doe', uid: '2', avatar: 'JD', hasVoted: true },
+    { name: 'John Smith', uid: '3', avatar: 'JS', hasVoted: false },
+    { name: 'Jane Smith', uid: '4', avatar: 'JS', hasVoted: true },
+    { name: 'Jane White', uid: '10', avatar: 'JW', hasVoted: false },
+  ];
+
+  colors = ['blue', 'green', 'red', 'yellow', 'purple', 'orange', 'pink', 'brown', 'black', 'white'];
+
+
+  constructor() {
+    IconRegistry.define([tylIconCheckCircle, tylIconRadioButtonUnchecked]);
+  }
+  getColor(index: number): string {
+    return this.colors[index % this.colors.length];
+  }
+}
+
+export interface IUser {
+  name: string;
+  uid: string;
+  avatar: string;
+  hasVoted: boolean;
 
 }
