8eaefe8da3138633fb05ae9650f5dca01548437f
ScottTylerTech <scott.shirley@tylertech.com>
2024-08-24 13:48:46 -0600

    refactor: Update room container component with voting buttons and styles

--- src/app/components/room/user-list/user-list.component.ts
+++ src/app/components/room/user-list/user-list.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ForgeButtonModule, ForgeCardModule } from '@tylertech/forge-angular';
+
+
+const forgeModules = [ ForgeButtonModule, ForgeCardModule];
+
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [...forgeModules],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
