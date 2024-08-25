a33ca2ab55d48d5cfd0a499bd2707d6abb97e304
ScottTylerTech <scott.shirley@tylertech.com>
2024-08-24 18:09:58 -0600

    chore: Update Firebase configuration and ignore .env files in gitignore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideFiebaseApp(() => initializeApp(firebaseConfig)),
+    provideFiebaseApp(() => initializeApp()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)]
 };
